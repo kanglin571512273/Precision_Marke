9476c93e6da704ba0e58c35679eec4f6bb43432e
taylor <taylorkanglin@gmail.com>
2020-11-27 14:32:02 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">
+      Dashed
+    </a-button>
+    <a-button type="danger">
+      Danger
+    </a-button>
+    <a-config-provider :auto-insert-space-in-button="false">
+      <a-button type="primary">
+        按钮
+      </a-button>
+    </a-config-provider>
+    <a-button type="primary">
+      按钮
+    </a-button>
+    <a-button type="link">
+      Link
+    </a-button>
+  </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Button } from 'ant-design-vue';
+Vue.use(Button);
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style lang="less" scoped>
+
+</style>
