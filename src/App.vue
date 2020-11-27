89eee09fa8770492e4fae341a87f809d56ac613c
zxf <1903044057@qq.com>
2020-11-27 14:36:13 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,7 @@
     <a-button type="danger">
       Danger
     </a-button>
-    <a-config-provider :auto-insert-space-in-button="false">
-      <a-button type="primary">
-        按钮
-      </a-button>
-    </a-config-provider>
-    <a-button type="primary">
-      按钮
-    </a-button>
-    <a-button type="link">
-      Link
-    </a-button>
+    
   </div>
   </div>
 </template>
